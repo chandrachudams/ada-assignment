e734fe5c22384c1a30442dd34bb03e8e7ffd0d13
chandrachudams <108355062+chandrachudams@users.noreply.github.com>
2022-06-29 14:43:19 +0530

    Update matrixmul.cpp

--- matrixmul.cpp
+++ matrixmul.cpp
@@ -1,68 +1,54 @@
-#include<iostream> 
+#include <bits/stdc++.h>
 using namespace std;
 
-void Mat(int d[],int n){
-int a[n][n],b[n],min,count=0; bool var[n];
-string eq="(";
-
-for(int i=0;i<n;i++)
+int MatrixChainOrder(int d[], int n)
 {
-var[i]=false; 
 
-for(int j=0;j<n-i;j++)
-{
-a[j][j+i]=9999; 
-for(int k=j;k<j+i;k++)
-{
-min=a[j][k]+a[k+1][j+i]+(d[j]*d[k+1]*d[j+i+1]); 
-if(min<a[j][j+i]) 
-{
-a[j][j+i]=min; if(j==0) b[j+i]=k;
-  }
-}
 
-if(i==0) a[j][j+i]=0;
-}
-}
+	int c[n][n];
 
-for(int i=n-1;i>0;i--) {
-var[b[i]+1]=true;
-}
+	int i, j, k, L, q;
 
-for(int i=1;i<n;i++){
-if(var[i])
-{
-eq+="a"+to_string(i)+")"; count++;
-}
-else eq+="a"+to_string(i);
-}
 
-for(int i=0;i<count;i++){
-eq="("+eq;
+	for (i = 1; i < n; i++)
+		c[i][i] = 0;
+
+
+	 for (L = 2; L < n; L++)
+    {
+        for (i = 1; i < n - L + 1; i++)
+        {
+            j = i + L - 1;
+            c[i][j] = INT_MAX;
+            for (k = i; k <= j - 1; k++)
+            {
+
+                q = c[i][k] + c[k + 1][j]
+                    + d[i - 1] * d[k] * d[j];
+                if (q < c[i][j])
+                    c[i][j] = q;
+            }
+        }
+    }
+
+	return c[1][n - 1];
 }
 
-eq+="a"+to_string(n)+")";
-cout<<endl<<"Minimum Multiplications: "<<a[0][n-1]<<endl; cout<<"The equation with minimum multiplications is: "<<eq<<endl;
-}
-
+// Driver Code
 int main()
 {
-int n;
-cout<<"Enter the number of matrices\n"; cin>>n;
-int a[n][2]; int d[n+1];
+	cout<<"Enter the size of the array\n";
+	int n;
+	cin>>n;
+    cout<<"Enter the array of the sizes of the matrices:\n";
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+	cout << "Minimum number of multiplications is "
+		<< MatrixChainOrder(arr,n);
 
-cout<<"Enter the size of the matrices in order\n";
-
-for(int i=0;i<n;i++) {
-cout<<"Matrix "<<i+1<<endl; cout<<"Row: ";
-cin>>a[i][0]; cout<<"Column: "; cin>>a[i][1];
+	return 0;
 }
 
-for(int i=0;i<=n;i++){
-if(i!=n) d[i]=a[i][0]; else
-d[i]=a[i-1][1];
-}
 
-Mat(d,n); 
-  return 0;
-}
